64973180f11612160b1a2c2acf5bf1af5aa2260e
volkrum123 <stiaanmegit2@gmail.com>
2025-08-11 13:28:54 +0200

    I have added the MVC folders to the file structure and i am dividing the primal simplex code into OOP code

---
+++ Models/LPModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LP_Solver.Models
+{
+    internal class LPModel
+    {
+        public string ObjectiveType { get; set; }
+        public List<int> ObjectiveCoefficients { get; set; }
+        public List<string> Constraints { get; set; }
+
+        public LPModel()
+        {
+            ObjectiveCoefficients = new List<int>();
+            Constraints = new List<string>();
+        }
+    }
+           
+}
