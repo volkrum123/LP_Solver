96221c84074cfc6e7af2c87ff25fc78c0f8e3f61
volkrum123 <stiaanmegit2@gmail.com>
2025-08-15 17:38:40 +0200

    Made changes to the form layout

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,33 +34,36 @@
             comboBox1 = new ComboBox();
             btnLoadFile = new Button();
             btnExport = new Button();
+            panel1 = new Panel();
+            label2 = new Label();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(757, 23);
+            label1.Location = new Point(284, 21);
             label1.Name = "label1";
-            label1.Size = new Size(106, 20);
+            label1.Size = new Size(176, 20);
             label1.TabIndex = 0;
-            label1.Text = "Type in model:";
+            label1.Text = "Manually Enter LP model:";
             // 
             // txtInput
             // 
-            txtInput.Location = new Point(631, 46);
+            txtInput.Location = new Point(284, 46);
             txtInput.Multiline = true;
             txtInput.Name = "txtInput";
             txtInput.ScrollBars = ScrollBars.Vertical;
-            txtInput.Size = new Size(400, 150);
+            txtInput.Size = new Size(743, 150);
             txtInput.TabIndex = 1;
             // 
             // txtOutput
             // 
             txtOutput.Font = new Font("Consolas", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            txtOutput.Location = new Point(64, 211);
+            txtOutput.Location = new Point(284, 251);
             txtOutput.Name = "txtOutput";
             txtOutput.ReadOnly = true;
-            txtOutput.Size = new Size(967, 306);
+            txtOutput.Size = new Size(743, 306);
             txtOutput.TabIndex = 3;
             txtOutput.Text = "";
             // 
@@ -70,7 +73,7 @@
             comboBox1.FlatStyle = FlatStyle.Flat;
             comboBox1.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(64, 23);
+            comboBox1.Location = new Point(5, 141);
             comboBox1.Name = "comboBox1";
             comboBox1.Size = new Size(240, 31);
             comboBox1.TabIndex = 4;
@@ -78,38 +81,58 @@
             // 
             // btnLoadFile
             // 
-            btnLoadFile.Location = new Point(349, 24);
+            btnLoadFile.Location = new Point(46, 44);
             btnLoadFile.Name = "btnLoadFile";
-            btnLoadFile.Size = new Size(94, 29);
+            btnLoadFile.Size = new Size(144, 29);
             btnLoadFile.TabIndex = 5;
-            btnLoadFile.Text = "Upload LP";
+            btnLoadFile.Text = "Upload LP model";
             btnLoadFile.UseVisualStyleBackColor = true;
             btnLoadFile.Click += btnLoadFile_Click;
             // 
             // btnExport
             // 
-            btnExport.Location = new Point(485, 528);
+            btnExport.Location = new Point(46, 89);
             btnExport.Name = "btnExport";
-            btnExport.Size = new Size(159, 29);
+            btnExport.Size = new Size(144, 29);
             btnExport.TabIndex = 6;
             btnExport.Text = "Export solved LP";
             btnExport.UseVisualStyleBackColor = true;
             btnExport.Click += btnExport_Click;
+            // 
+            // panel1
+            // 
+            panel1.BorderStyle = BorderStyle.FixedSingle;
+            panel1.Controls.Add(btnLoadFile);
+            panel1.Controls.Add(comboBox1);
+            panel1.Controls.Add(btnExport);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(250, 566);
+            panel1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(285, 214);
+            label2.Name = "label2";
+            label2.Size = new Size(178, 20);
+            label2.TabIndex = 8;
+            label2.Text = "Desplayed Solved Model:";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1085, 569);
-            Controls.Add(btnExport);
-            Controls.Add(btnLoadFile);
-            Controls.Add(comboBox1);
+            Controls.Add(label2);
+            Controls.Add(panel1);
             Controls.Add(txtOutput);
             Controls.Add(txtInput);
             Controls.Add(label1);
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load_1;
+            panel1.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -123,5 +146,7 @@
         private ComboBox comboBox1;
         private Button btnLoadFile;
         private Button btnExport;
+        private Panel panel1;
+        private Label label2;
     }
 }
